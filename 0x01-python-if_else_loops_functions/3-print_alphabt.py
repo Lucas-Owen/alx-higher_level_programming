7a8ed66c64edd0cb1d59189ab7e308861d810ea5
= <=>
2023-06-06 18:23:41 +0300

    Use format string

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(27):
+for i in range(26):
     if ord('a') + i == ord('e') or ord('a') + i == ord('q'):
         continue
-    print(chr(ord('a') + i), end="")
+    print('{}'.format(chr(ord('a') + i)), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for c in str:
         if ord(c) <= ord('z') and ord(c) >= ord('a'):
             c = chr(ord('A') + ord(c) - ord('a'))
-        print(c, end="")
+        print('{}'.format(c), end="")
     print()
