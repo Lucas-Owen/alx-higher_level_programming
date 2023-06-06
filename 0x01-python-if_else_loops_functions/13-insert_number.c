ceebdcab1b65b5bbd35358d91fba4f55c97b215a
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-06 13:08:53 +0300

    Add 9-12.py, 13-*.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert a node to a sorted linked list
+ * @head: Head of the linked list
+ * @number: value of the node
+ * Return: pointer to the new node or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *prev = NULL, *curr;
+
+	if (head == NULL)
+		return (NULL);
+	curr = *head;
+	while (curr)
+	{
+		if (number > curr->number)
+		{
+			prev = curr;
+			curr = curr->next;
+			continue;
+		}
+		break;
+	}
+	if (prev == NULL)
+		return (add_nodeint_end(head, number));
+	prev->next = NULL;
+	if (add_nodeint_end(&prev->next, number))
+		prev->next->next = curr;
+	return (prev->next);
+}
