ad629731099ad7a5c800e8c980fbe9571de50eb4
= <=>
2023-06-08 17:10:58 +0300

    Fix bug on checking operator symbol

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
         print(res.format(a, operator, b, add(a, b)))
     elif operator == '-':
         print(res.format(a, operator, b, sub(a, b)))
-    elif operator == '/':
-        print(res.format(a, operator, b, mul(a, b)))
     elif operator == '*':
+        print(res.format(a, operator, b, mul(a, b)))
+    elif operator == '/':
         print(res.format(a, operator, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
