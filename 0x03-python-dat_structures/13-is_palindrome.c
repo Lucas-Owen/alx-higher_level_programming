a84a89f05b57e63068fa0bf6e8731d240e444cb1
= <=>
2023-06-09 15:44:29 +0300

    Make *.py executable

--- 0x03-python-dat_structures/13-is_palindrome.c
+++ 0x03-python-dat_structures/13-is_palindrome.c
@@ -8,32 +8,35 @@
  */
 int is_palindrome_iterative(listint_t **head)
 {
-	listint_t *rev_head, *curr, *temp;
+	listint_t *rev_head = NULL, *curr, *temp = NULL;
 	int len = 1;
 
 	if (head == NULL || *head == NULL)
 		return (0);
-	rev_head = *head;
-	curr = rev_head->next;
-	rev_head->next = NULL;
-	/* Reverse the list */
+	add_nodeint_end(&rev_head, (*head)->n);
+	curr = (*head)->next;
+	/* Reverse copy the list */
 	while (curr)
 	{
-		temp = curr->next;
-		curr->next = rev_head;
-		rev_head = curr;
-		curr = temp;
+		add_nodeint_end(&temp, curr->n);
+		temp->next = rev_head;
+		rev_head = temp;
+		curr = curr->next;
+		temp = NULL;
 		++len;
 	}
 	temp = *head;
+	curr = rev_head;
 	len /= 2;
 	while (len >= 0)
 	{
-		if (temp->n != rev_head->n)
+		if (temp->n != curr->n)
 			return (0);
 		temp = temp->next;
-		rev_head = rev_head->next;
+		curr = curr->next;
+		len--;
 	}
+	free_listint(rev_head);
 
 	return (1);
 }
@@ -50,7 +53,7 @@ int is_palindrome_recursive(listint_t **head, listint_t *tail)
 	if (tail == NULL)
 		return (1);
 	is_palindrome_recursive(head, tail->next);
-	if (head->n != tail->n)
+	if ((*head)->n != tail->n)
 		return (0);
 	*head = (*head)->next;
 	return (1);
