ea7fedfbea15691149b5f4c0777b24b5ec1992fe
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-09 11:17:05 +0300

    Add 4-new_in_list.py

---
+++ 0x03-python-dat_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specified position
+        without modifying the original list
+    args:
+        my_list - The list
+        idx - index to replace
+        element - new element to be inserted
+    """
+    if idx < 0 or idx >= len(my_list):
+        return list(my_list)
+    l2 = list(my_list)
+    l2.replace(idx, element)
+    return l2
