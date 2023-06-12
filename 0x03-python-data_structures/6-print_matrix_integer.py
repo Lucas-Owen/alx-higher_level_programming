28c2159389a94b1545a9638c7e7644e026a65e21
= <=>
2023-06-12 18:13:30 +0300

    Change format for None and empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
+    if matrix is None or not matrix[0]:
+        print()
         return
     for row in matrix:
         for i, col in enumerate(row):
