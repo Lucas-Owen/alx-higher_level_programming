b19679aefcefc8bcd4d8ba7c96ded57c2c69bace
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-12 11:52:05 +0300

    Change output format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print('{:d}'.format(row), end='')
-        print('{:s}'.format('\n'))
+        print('{:s}'.format('\n'), end='')
