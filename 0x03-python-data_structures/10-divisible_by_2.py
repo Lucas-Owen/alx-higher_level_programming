fdf4c49ded36d70bf53e16ab112692efe7fe4d47
= <=>
2023-06-12 17:51:36 +0300

    Comply with pycodestyle

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,10 @@
 def divisible_by_2(my_list=[]):
     """Returns a new list same length as my_list with True False
     for numbers divisible or not divisible by 2
-    
     args:
-    my_list (list): List of integers
-    
-    Returns:
-    list: A list containing True and False corresponding to the
-    indices of numbers divisible and not divisible by 2 in my_list
+        my_list (list): List of integers
+    Returns: list
+        A list containing True and False corresponding to the
+        indices of numbers divisible and not divisible by 2 in my_list
     """
     return [x % 2 == 0 for x in my_list]
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     """Delete an element in a list
-    
     Args:
         my_list (list): The list
         idx (int): Index to delete at
-    
     Returns: list
         A modified list
     """
