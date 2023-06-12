a5a92c69abaf44ec062689ce03bbde19ee4af4ee
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-12 12:02:11 +0300

    Accomodate empty tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    list_a = [0, 0]
-    list_b = [0, 0]
-    for i, a, b in enumerate(zip(tuple_a, tuple_b)):
-        list_a[i] = a
-        list_b[i] = b
-        if i == 1:
-            break
-    return tuple(list_a[0] + list_b[0], list_a[1] + list_b[1])
+    result_list = [0, 0]
+    for i, a in zip(range(2), tuple_a):
+        result_list[i] += a
+    for i, b in zip(range(2), tuple_b):
+        result_list[i] += b
+    return tuple(result_list)
