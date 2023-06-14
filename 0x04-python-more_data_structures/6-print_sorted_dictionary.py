80cfec4196d250f46ff7a864ef70b4df089e523d
= <=>
2023-06-14 18:51:01 +0300

    Replace for with map

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,3 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
         for key in sorted(a_dictionary):
             print(f'{key} : {a_dictionary[key]}')
+    else:
+        print()
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
-        return {key : value ** 2 for key, value in a_dictionary.items()}
+        return {key: value ** 2 for key, value in a_dictionary.items()}
