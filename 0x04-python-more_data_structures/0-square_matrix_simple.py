6a93865a9b7c861c04e84621c98b2c7ec62dc829
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-14 09:53:26 +0300

    Add 2-uniq_add.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return None
-    return [[i**2 for i in row] for row in matrix]
+    if matrix is not None:
+        return [[i**2 for i in row] for row in matrix]
