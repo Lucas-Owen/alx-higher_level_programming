27c016881a82f6e1563b9235ecd0cc66d0cdc215
= <=>
2023-06-14 19:05:21 +0300

    Add case for empty or None

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     total = 0
     weight = 0
+    if my_list is None or not my_list:
+        return 0
     for pair in my_list:
         total += pair[0] * pair[1]
         weight += pair[1]
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    if roman_string is None or not roman_string:
+    if roman_string is None or not roman_string or type(roman_string) != str:
         return 0
     roman_string = roman_string.upper()
     invalid = [c for c in roman_string if c not in 'IVXLCD']
