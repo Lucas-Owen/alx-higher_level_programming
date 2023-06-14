20007d6065c3649fa824d104156435a0efbbb211
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-14 09:49:11 +0300

    Add 1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
-    return [[i**2 for row in matrix] for i in row]
+    return [[i**2 for i in row] for row in matrix]
