7405a5f3453c319d68e13b73bc4e599bb1c1fc18
= <=>
2023-06-14 18:55:08 +0300

    Change output format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and a_dictionary:
         for key in sorted(a_dictionary):
-            print(f'{key} : {a_dictionary[key]}')
-    else:
-        print()
+            print(f'{key}: {a_dictionary[key]}')
