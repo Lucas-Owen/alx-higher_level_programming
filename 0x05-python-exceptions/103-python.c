35a6e6a483673917d9953b79b4cc173ab9b169a9
= <=>
2023-06-26 21:11:05 +0300

    Change output format

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -12,6 +12,7 @@ void print_python_bytes(PyObject *p)
 	Py_ssize_t i;
 	char *s;
 
+	setbuf(stdout, NULL);
 	puts("[.] bytes object info");
 	if (p == NULL || strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
@@ -32,7 +33,6 @@ void print_python_bytes(PyObject *p)
 		else
 			putchar('\n');
 	}
-	fflush(stdout);
 }
 
 /**
@@ -45,6 +45,7 @@ void print_python_float(PyObject *p)
 	char buf[99];
 	unsigned int len, i;
 
+	setbuf(stdout, NULL);
 	puts("[.] float object info");
 	if (p == NULL || strcmp(p->ob_type->tp_name, "float") != 0)
 	{
@@ -58,7 +59,6 @@ void print_python_float(PyObject *p)
 		buf[i] = '\0';
 	}
 	printf("  value: %s\n", buf);
-	fflush(stdout);
 }
 
 
@@ -72,12 +72,13 @@ void print_python_list(PyObject *p)
 	Py_ssize_t i;
 	PyListObject *obj;
 
+	setbuf(stdout, NULL);
+	puts("[*] Python list info");
 	if (p == NULL || strcmp(p->ob_type->tp_name, "list") != 0)
 	{
 		puts("  [ERROR] Invalid List Object");
 		return;
 	}
-	puts("[*] Python list info");
 	printf("[*] Size of the Python List = %lu\n",
 		((PyVarObject *) p)->ob_size);
 	printf("[*] Allocated = %lu\n", ((PyListObject *) p)->allocated);
@@ -91,5 +92,4 @@ void print_python_list(PyObject *p)
 		else if (strcmp(obj->ob_item[i]->ob_type->tp_name, "float") == 0)
 			print_python_float(obj->ob_item[i]);
 	}
-	fflush(stdout);
 }
