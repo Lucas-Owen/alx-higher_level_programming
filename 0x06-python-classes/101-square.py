7c2fb1763dbced4f596cc51a815a543e299da99d
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-28 08:27:49 +0300

    Change 'two' to 2

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -72,7 +72,7 @@ class Square:
                 type(value[0]) is not int or type(value[1]) is not int or
                 value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple"
-                            " of two positive integers")
+                            " of 2 positive integers")
         self.__position = value
 
     def my_print(self):
