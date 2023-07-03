283bc81ad20c05157e3552dc75dfc672dc267ee1
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-07-03 11:15:02 +0300

    Add 0, 1, 2, 3-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a rectangle class
+"""
+
+
+class Rectangle():
+    """A Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This module defines a rectangle class
+"""
+
+
+class Rectangle():
+    """A Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initialize with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Sets the width
+
+        Args:
+            value (int): New width
+        Raises:
+            TypeError
+            ValueError
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """Sets the height
+
+        Args:
+            value (int): New height
+        Raises:
+            TypeError
+            ValueError
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of this Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of this Rectangle"""
+        return 2 * self.width * self.height
+
+    def __str__(self):
+        """Returns a string representation of this Rectangle"""
+        res = ('#' * self.width + '\n') * (self.height - 1)
+        res += '#' * self.width
+        return res
