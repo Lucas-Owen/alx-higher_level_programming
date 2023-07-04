72afca4fa02073930b98ab9da79e88c34d3e8d16
= <=>
2023-07-04 15:59:22 +0300

    Use slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-class LockedClass():
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            return super().__setattr__(name, value)
-        raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+"""This module defines a class LockedClass"""
+
+
+class LockedClass:
+    """LockedClass class"""
+    __slots__ = ['first_name']
