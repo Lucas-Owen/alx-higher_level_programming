0c77e3205a07b72a401ba2b198a0edd90bf5bce7
= <=>
2023-07-10 21:09:33 +0300

    Stop using issubclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,4 @@ is only an instance of an inherited class
 
 def inherits_from(obj, a_class):
     """Checks if an object is only a subclass of a_class"""
-    # This also works
-    # return type(obj) is not a_class and isinstance(obj, a_class)
-    return issubclass(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
