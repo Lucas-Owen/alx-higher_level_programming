027f57943e8bd3a9938b71fe44bf0204adaa59e2
= <=>
2023-07-10 21:13:16 +0300

    Set defaults to None

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,7 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
-    def __init__(self, width=1, height=1):
+    def __init__(self, width=None, height=None):
         """Initializer with width and height"""
         BaseGeometry.integer_validator(self, 'width', width)
         BaseGeometry.integer_validator(self, 'height', height)
