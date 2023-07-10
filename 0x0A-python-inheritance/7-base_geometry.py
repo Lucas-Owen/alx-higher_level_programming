56aa8137e341921b60d79d79de94963587458b4c
= <=>
2023-07-10 21:04:35 +0300

    Change output in tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         """Not implemented"""
         raise Exception('area() is not implemented')
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name='', value=None):
         """Validates that value is an integer"""
         if type(value) is not int:
             raise TypeError(f'{name} must be an integer')
