045df3e138bba5aaa1214f16a6addfca0d95fdd6
Lucas Sangura <sanguraowens@gmail.com>
2023-09-29 21:07:58 +0300

    Use tabs before -

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
     print('Body response:')
-    print(f'    - type: {type(body)}')
-    print(f'    - content: {body}')
-    print(f'    - utf8 content: {body.decode()}')
+    print(f'\t- type: {type(body)}')
+    print(f'\t- content: {body}')
+    print(f'\t- utf8 content: {body.decode()}')
