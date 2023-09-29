27342d2486189bacc647fb5ac38facef18bb0b9a
Lucas Sangura <sanguraowens@gmail.com>
2023-09-29 21:30:39 +0300

    Comply with pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
+    q = '' if len(argv) == 1 else argv[1]
     head = {'Allow': 'application/json'}
-    data = {'q': argv[1]}
+    data = {'q': q}
     url = 'http://0.0.0.0:5000/search_user'
 
     try:
-        with requests.request('POST', url=url, headers=head, data=data) as resp:
-            resp.raise_for_status()
-            usr = resp.json()
+        with requests.request('POST', url=url, headers=head, data=data) as res:
+            res.raise_for_status()
+            usr = res.json()
             if usr:
                 print(f'[{usr.get("id", None)}] {usr.get("name", None)}')
             else:
