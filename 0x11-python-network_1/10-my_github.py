9a9d9f5fc6830c57a8941730c5500636c8fbb342
Lucas Sangura <sanguraowens@gmail.com>
2023-09-29 21:33:15 +0300

    Only run when __main__

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,22 +5,23 @@ and uses the GitHub API to display your id
 """
 
 
-import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
 
-username = argv[1]
-token = argv[2]
-auth = (username, token)
-url = f'https://api.github.com/user'
-headers = {
-    'Accept': 'application/json',
-    'X-GitHub-Api-Version': '2022-11-28'
-}
+    username = argv[1]
+    token = argv[2]
+    auth = (username, token)
+    url = f'https://api.github.com/user'
+    headers = {
+        'Accept': 'application/json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
 
-try:
-    with requests.get(url=url, headers=headers, auth=auth) as response:
-        response.raise_for_status()
-        print(response.json()['id'])
-except Exception as e:
-    print('None')
+    try:
+        with requests.get(url=url, headers=headers, auth=auth) as response:
+            response.raise_for_status()
+            print(response.json()['id'])
+    except Exception as e:
+        print('None')
